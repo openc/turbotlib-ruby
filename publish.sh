565b10ac8c242b46f71bff781a692afc48f1d802
Seb Bacon <seb.bacon@gmail.com>
2014-06-30 15:28:49 +0100

    Check no unpushed git commits in publish script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-gem build turbotlib.gemspec
-gem push $(ls *gem|tail -1)
+UNPUSHED=$(git log origin/master..HEAD)
+if [ "x$UNPUSHED" == "x" ]; then
+    gem build turbotlib.gemspec
+    gem push $(ls *gem|tail -1)
+fi
 
 function clean {
  rm *gem
