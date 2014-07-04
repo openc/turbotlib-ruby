e45aa26fad63210c25bf4dba06a74a713e3a228e
Peter Inglesby <peter.inglesby@gmail.com>
2014-07-04 10:22:13 +0100

    Add get_var and save_var

--- lib/turbotlib.rb
+++ lib/turbotlib.rb
@@ -1,9 +1,13 @@
+require 'yaml'
+
 module Turbotlib
-  def self.log(message)
+  extend self
+
+  def log(message)
     $stderr.puts message
   end
 
-  def self.data_dir
+  def data_dir
     if in_production?
       "/data"
     else
@@ -11,9 +15,44 @@ module Turbotlib
     end
   end
 
+  def set_up_data_dir
+    begin
+      Dir.mkdir(data_dir)
+    rescue Errno::EEXIST
+    end
+  end
+
+  def save_var(key, val)
+    vars = get_vars
+    vars[key] = val
+    save_vars(vars)
+    val
+  end
+
+  def get_var(key)
+    get_vars[key]
+  end
+
   private
-  def self.in_production?
+  def in_production?
     !!ENV['MORPH_URL']
   end
 
+  def get_vars
+    set_up_data_dir
+    begin
+      YAML.load_file(vars_path)
+    rescue Errno::ENOENT
+      {}
+    end
+  end
+
+  def save_vars(vars)
+    set_up_data_dir
+    File.open(vars_path, 'w') {|f| YAML.dump(vars, f)}
+  end
+
+  def vars_path
+    "#{data_dir}/_vars.yml"
+  end
 end
