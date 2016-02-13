06623a3febcc90d4dd1771014ae0f81af99e7451
James McKinney <james@slashpoundbang.com>
2016-02-12 21:58:55 -0500

    Split spec file into spec_helper.rb an turbotlib_spec.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+
+require "simplecov"
+# require "coveralls"
+# SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter "spec"
+end
+
+require 'rspec'
+require File.dirname(__FILE__) + '/../lib/turbotlib'
+
+require 'fileutils'
--- spec/turbotlib_spec.rb
+++ spec/turbotlib_spec.rb
@@ -1,89 +1,74 @@
-require "simplecov"
-require "coveralls"
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start do
-  add_filter "spec"
-end
-
-require 'fileutils'
-
-require 'turbotlib'
-
 describe Turbotlib do
-  describe "log" do
-    it "should print message" do
+  describe '.log' do
+    it 'should print message' do
       expect {
-        Turbotlib.log("hi")
+        Turbotlib.log('hi')
       }.to output("hi\n").to_stderr
     end
   end
 
-  describe "data_dir" do
-    context "when developing" do
+  describe '.data_dir' do
+    context 'when developing' do
       after do
-        FileUtils.rm_rf("data")
+        FileUtils.rm_rf('data')
       end
 
-      it "should return data" do
-        expect(Turbotlib.data_dir).to eq("data")
+      it 'should return data' do
+        expect(Turbotlib.data_dir).to eq('data')
       end
 
-      it "should create data directory" do
+      it 'should create data directory' do
         Turbotlib.data_dir
-        expect(File.exists?("data")).to eq(true)
+        expect(File.exists?('data')).to eq(true)
       end
     end
 
-    context "when running in morph" do
+    context 'when running in morph' do
       before do
-        allow(ENV).to receive(:[]).with("MORPH_URL").and_return("something")
+        allow(ENV).to receive(:[]).with('MORPH_URL').and_return('something')
       end
 
-      it "should return /data" do
-        expect(Turbotlib.data_dir).to eq("/data")
+      it 'should return /data' do
+        expect(Turbotlib.data_dir).to eq('/data')
       end
 
-      it "should not create data directory" do
-        expect(File.exists?("data")).to eq(false)
+      it 'should not create data directory' do
+        expect(File.exists?('data')).to eq(false)
       end
     end
   end
 
-  describe "sources_dir" do
-    context "when developing" do
+  describe '.sources_dir' do
+    context 'when developing' do
       after do
-        FileUtils.rm_rf("sources")
+        FileUtils.rm_rf('sources')
       end
 
-      it "should return sources" do
-        expect(Turbotlib.sources_dir).to eq("sources")
-      end
-
-      it "should create sources directory" do
-        Turbotlib.sources_dir
-        expect(File.exists?("sources")).to eq(true)
+      it 'should return sources and create the directory' do
+        expect(Turbotlib.sources_dir).to eq('sources')
+        expect(File.exists?('sources')).to eq(true)
       end
     end
 
-    context "when running in morph" do
-      context "and not an admin" do
-        before do
-          allow(Turbotlib).to receive(:in_production?).and_return(true)
-        end
+    context 'when running in morph' do
+      before do
+        allow(Turbotlib).to receive(:in_production?).and_return(true)
+      end
 
-        it "should raise exception" do
-          expect{Turbotlib.sources_dir}.to raise_error
+      context 'and not an admin' do
+        it 'should raise exception' do
+          expect{Turbotlib.sources_dir}.to raise_error("Only admins are permitted to write to `sources_dir`")
         end
       end
 
-      context "and an admin" do
+      context 'and an admin' do
         before do
-          allow(Turbotlib).to receive(:in_production?).and_return(true)
           allow(Turbotlib).to receive(:is_admin?).and_return(true)
         end
 
-        it "should return /sources" do
-          expect(Turbotlib.sources_dir).to eq("/sources")
+        it 'should return /sources and not create the directory' do
+          expect(Turbotlib.sources_dir).to eq('/sources')
+          expect(File.exists?('/sources')).to eq(false)
         end
       end
     end
