8f500b6d4d4aa7ad339ded8638446ebbef37d133
James McKinney <james@slashpoundbang.com>
2016-02-12 21:58:17 -0500

    Document methods and make .in_production? public

--- lib/turbotlib.rb
+++ lib/turbotlib.rb
@@ -1,26 +1,41 @@
 require 'yaml'
+
 require 'scraperwiki'
 
 class Turbotlib
   class << self
     include ScraperWiki
 
+    # Logs a message to STDERR.
+    #
+    # @param [String] message a log message
     def log(message)
       $stderr.puts message
     end
 
+    # Returns the path to the data directory.
+    #
+    # @return [String] the path to the data directory
     def data_dir
-      path_to("data")
+      path_to('data')
     end
 
+    # Returns the path to the sources directory.
+    #
+    # @return [String] the path to the sources directory
     def sources_dir
       if in_production? && !is_admin?
-        raise "Only admins are permitted to write to `sources_dir`"
+        raise 'Only admins are permitted to write to `sources_dir`'
       else
-        path_to("sources")
+        path_to('sources')
       end
     end
 
+    # Saves and returns the value of a variable.
+    #
+    # @param [String] a variable name
+    # @param val the value of the variable
+    # @return the value of the variable
     def save_var(key, val)
       vars = get_vars
       vars[key] = val
@@ -28,23 +43,33 @@ class Turbotlib
       val
     end
 
+    # Returns the value of a variable.
+    #
+    # @param [String] a variable name
+    # @return the value of the variable
     def get_var(key)
       get_vars[key]
     end
 
-    # Override default in ScraperWiki gem
+    # Override default in ScraperWiki gem.
+    #
+    # @return [SqliteMagic::Connection] a SQLite connection
     def sqlite_magic_connection
       db = "#{data_dir}/data.sqlite"
       @sqlite_magic_connection ||= SqliteMagic::Connection.new(db)
     end
 
-    private
+    # Returns whether the environment is production.
+    #
+    # @return [Boolean] whether the environment is production
     def in_production?
       !!ENV['MORPH_URL']
     end
 
+  private
+
     def is_admin?
-      ENV['USER_ROLES'].to_s.split(",").include?("admin")
+      ENV['USER_ROLES'].to_s.split(',').include?('admin')
     end
 
     def get_vars
@@ -66,18 +91,14 @@ class Turbotlib
 
     def path_to(dir)
       if in_production?
-        location = "/#{dir}"
+        "/#{dir}"
       else
-        location = dir
-        set_up_dir(location)
-      end
-      location
-    end
+        begin
+          Dir.mkdir(dir)
+        rescue Errno::EEXIST
+        end
 
-    def set_up_dir(location)
-      begin
-        Dir.mkdir(location)
-      rescue Errno::EEXIST
+        dir
       end
     end
   end
